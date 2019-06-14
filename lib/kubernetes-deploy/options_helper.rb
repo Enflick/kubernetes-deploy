6b0e4a3b5ca3fcf92fb8eda1b00f050e91d5923a
Emmanuel dos Santos <manel.pb@gmail.com>
2019-06-14 18:11:40 -0400

    Fix yaml comment

--- lib/kubernetes-deploy/options_helper.rb
+++ lib/kubernetes-deploy/options_helper.rb
@@ -5,11 +5,16 @@ module KubernetesDeploy
     def self.default_and_check_template_dir(template_dir)
       if !template_dir && ENV.key?("ENVIRONMENT")
         template_dir = "config/deploy/#{ENV['ENVIRONMENT']}"
+
+        # make sure directory exists before trying to use when the ENV is set
+        if (not File.exists? template_dir)
+          template_dir = nil
+        end
       end
 
       if !template_dir || template_dir.empty?
         template_dir = "."
-        puts "-- [info] Using current directory as template directory"
+        puts "# [info] Using current directory as template directory"
       end
 
       template_dir
@@ -17,7 +22,7 @@ module KubernetesDeploy
 
     def self.revision_from_environment
       ENV.fetch('REVISION') do
-        puts "-- [info] Using 1 as default Revision"
+        puts "# [info] Using 1 as default Revision"
         '1'
       end
     end
